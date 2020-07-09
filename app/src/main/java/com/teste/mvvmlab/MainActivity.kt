054dcf5101f25c316eda7fd20f8910bca80cb882
gabrielguedeshhsurge <gabriel.guedes@hhsurge.com>
2020-07-09 17:55:17 +0100

    Now there are two fragments (Fragment_A.kt and Fragment_B.kt) sharing the same ViewModel

--- app/src/main/java/com/teste/mvvmlab/MainActivity.kt
+++ app/src/main/java/com/teste/mvvmlab/MainActivity.kt
@@ -1,21 +1,12 @@
 package com.teste.mvvmlab
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
-import androidx.databinding.DataBindingUtil
-import com.teste.mvvmlab.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    private val mainViewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
-            R.layout.activity_main)
-
-        binding.viewModel = mainViewModel
-        binding.lifecycleOwner = this
+        setContentView(R.layout.activity_main)
     }
 }
