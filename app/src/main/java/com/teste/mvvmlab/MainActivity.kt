2ccaf47207d30c28d7e2819b5f39e923bb9524ed
Pedro Carvalho <pedro.carvalho@hhsurge.com>
2020-07-09 13:31:58 +0100

    finish project

--- app/src/main/java/com/teste/mvvmlab/MainActivity.kt
+++ app/src/main/java/com/teste/mvvmlab/MainActivity.kt
@@ -2,11 +2,20 @@ package com.teste.mvvmlab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import com.teste.mvvmlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+
+        binding.viewModel = mainViewModel
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/teste/mvvmlab/MainViewModel.kt
+++ app/src/main/java/com/teste/mvvmlab/MainViewModel.kt
@@ -1,6 +1,22 @@
 package com.teste.mvvmlab
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel: ViewModel() {
+
+    private var number = 1
+
+    private val _counter = MutableLiveData<String>()
+    val counter: LiveData<String>
+         get() = _counter
+
+    init {
+        _counter.value = number.toString()
+    }
+
+    fun sumNumbers(){
+        _counter.value = (++number).toString()
+    }
 }
